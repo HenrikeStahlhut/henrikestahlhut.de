4002dc34052f59a8d395e5fb55612cb541417419
Henrike Stahlhut <henrike.st@icloud.com>
2023-08-20 19:59:12 +0200

    fix: margin on projects text

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -4,9 +4,10 @@ import Background from "@/components/Background/Background";
 import Layout from "@/components/Layout/Layout";
 import Subheadline from "@/components/Subheadline/Subheadline";
 import { styled } from "styled-components";
+import { Container, StyledText } from "@/components/About/About";
 
 const StyledMarginContainer = styled.div`
-  margin: 50px;
+  margin: 50px auto;
 `;
 
 export default function Projects() {
@@ -16,9 +17,9 @@ export default function Projects() {
 
       <Layout>
         <Header />
-        <StyledMarginContainer>
+        <Container>
           <Subheadline>This page is under construction...</Subheadline>
-        </StyledMarginContainer>
+        </Container>
       </Layout>
       <Footer />
     </>
